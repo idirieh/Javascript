2c10cbfd41f2443e96c9fbe99ad82cc070ea4fd3
Ismail <ismael.dirieh@gmail.com>
2024-12-15 12:58:01 +0300

    add some files to axios

--- Tarmeez/Advance-JS/Axios/app.js
+++ Tarmeez/Advance-JS/Axios/app.js
@@ -32,10 +32,10 @@ function getUsers() {
 
 }
     
-getUsers()
-.then(() => {
-    getPosts()
-})
+// getUsers()
+// .then(() => {
+//     getPosts()
+// })
 
 function getPosts(id=1) {
     fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
@@ -54,8 +54,9 @@ function getPosts(id=1) {
             }
         })        
 }
+
 function userClicked(id, e){
-    getPosts(id)
+    getPostsWithAxios(id)
     let activeElement = document.getElementsByClassName('active');
     for(element of activeElement){
         element.classList.remove('active')
@@ -63,3 +64,57 @@ function userClicked(id, e){
     e.parentElement.classList.add('active')
 }
 
+
+// Sart from here with Axios
+function getUsersUsingAxios(){
+    return new Promise((resolve, reject) => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+            usersInfo.innerHTML = "";
+            let users = response.data;
+            for(user of users){
+                let content = `
+                    <div class="user-info">
+                         <h3 id="user-name">${user.name}</h3>
+                         <p id="user-email">${user.email}</p>
+                         <button class="btn" onclick="userClicked(${user.id}, this)">Get Details</button>
+                     </div>
+                `
+                usersInfo.innerHTML += content;
+            } 
+            resolve()
+        })
+        .catch(error => {
+            // alert(error)
+            reject(error)
+        })
+    })
+
+}
+
+// so we can call the posts after we called the users
+getUsersUsingAxios()
+.then(() => {
+    getPostsWithAxios()
+}).catch((e) => {
+    alert(e)
+})
+
+function getPostsWithAxios(id=2 ) {
+    let url = `https://jsonplaceholder.typicode.com/posts?userId=${id}`;
+    axios.get(url)
+    .then((response) => {
+        userPosts.innerHTML = ""
+        let posts = response.data;
+        for(post of posts) {
+            let content = `
+                <div class="post">
+                    <h3 id="post-title">${post.title}</h3>
+                    <hr>
+                    <p id="post-body">${post.body}</p>
+                </div>
+            `
+            userPosts.innerHTML += content
+        }
+    })    
+}
